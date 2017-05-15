b578dadd2108d48fe970dce0c5502f13e9bd5f22
René Bärnreuther! <rene.baernreuther@web.de>
2017-05-15 17:55:41 +0200

    Implemented more complex example of BPMNModele

--- master.project/src/main/java/ai4/master/project/process/ProcessModelerImpl.java
+++ master.project/src/main/java/ai4/master/project/process/ProcessModelerImpl.java
@@ -3,13 +3,6 @@ package ai4.master.project.process;
 import ai4.master.project.recipe.Recipe;
 import org.camunda.bpm.model.bpmn.Bpmn;
 import org.camunda.bpm.model.bpmn.BpmnModelInstance;
-import org.camunda.bpm.model.bpmn.instance.*;
-import org.camunda.bpm.model.bpmn.instance.Process;
-
-import java.io.File;
-import java.io.IOException;
-
-import static org.camunda.bpm.model.xml.test.AbstractModelElementInstanceTest.modelInstance;
 
 /**
  * Created by René Bärnreuther on 15.05.2017.
@@ -19,41 +12,45 @@ public class ProcessModelerImpl implements ProcessModeler {
 
     private BpmnModelInstance modelInstance;
 
+    public static void main(String[] args) {
+
+    }
+
     /**
      * Returns an empty instance currently
      * @param recipe The recipe containing the steps annd other informations for convesion
      * @return
      */
     public BpmnModelInstance convertToProcess(Recipe recipe) {
-        this.modelInstance = Bpmn.createProcess("Test").name("TestProzess").startEvent().userTask("Kochen").name("Some cooking to do").endEvent().done();
+        //this.modelInstance = Bpmn.createProcess("Test").name("TestProzess").startEvent().userTask("Kochen").name("Some cooking to do").endEvent().done();
+        //System.out.println(createXml());
+        // System.out.println("---");
+
+        // Be careful: When opening the xml in Camunda Modeler, "Zwiebeln" and "Knoblauch"- schneiden will overlap.
+        this.modelInstance = Bpmn.createProcess()
+                .name("Kochrezept")
+                .startEvent()
+                .userTask()
+                .name("Sachen bereitstellen")
+                .parallelGateway("parallel")
+                .userTask("zwiebeln")
+                .name("Zwieben schneiden")
+                .moveToNode("parallel")
+                .userTask("knoblauch")
+                .name("Knoblauch schneiden")
+                .parallelGateway("parallel_end")
+                .moveToActivity("zwiebeln")
+                .connectTo("parallel_end")
+                .moveToActivity("knoblauch")
+                .connectTo("parallel_end")
+                .moveToNode("parallel_end")
+                .userTask("Anschwitzen")
+                .name("Beides anschwitzen")
+                .endEvent()
+                .done();
 
         System.out.println(createXml());
-
         return modelInstance;
-    }
-    private <T extends BpmnModelElementInstance> T createElement(BpmnModelElementInstance parentElement, String id, Class<T> elementClass) {
-        T element = modelInstance.newInstance(elementClass);
-        element.setAttributeValue("id", id, true);
-        parentElement.addChildElement(element);
-        return element;
-    }
-
-    private SequenceFlow createSequenceFlow(Process process, FlowNode from, FlowNode to) {
-        String identifier = from.getId() + "-" + to.getId();
-        SequenceFlow sequenceFlow = this.createElement(process, identifier, SequenceFlow.class);
-        process.addChildElement(sequenceFlow);
-        sequenceFlow.setSource(from);
-        from.getOutgoing().add(sequenceFlow);
-        sequenceFlow.setTarget(to);
-        to.getIncoming().add(sequenceFlow);
-        return sequenceFlow;
-    }
-
-    private Definitions setDefinitions(BpmnModelInstance modelInstance){
-        Definitions definitions = modelInstance.newInstance(Definitions.class);
-        definitions.setTargetNamespace("http://camunda.org/examples");
-        modelInstance.setDefinitions(definitions);
-        return definitions;
     }
 
     /**
@@ -63,11 +60,5 @@ public class ProcessModelerImpl implements ProcessModeler {
     public String createXml(){
         Bpmn.validateModel(modelInstance);
         return Bpmn.convertToString(modelInstance);
-    }
-
-
-    public static void main(String[] args){
-        ProcessModelerImpl processModeler = new ProcessModelerImpl();
-        processModeler.convertToProcess(new Recipe());
     }
 }
