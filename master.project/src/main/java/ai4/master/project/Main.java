8a2a297b6cd79d8fab7decf9801585ee3783e979
René Bärnreuther! <rene.baernreuther@web.de>
2017-05-18 16:44:43 +0200

    LANGUAGE ENUM

--- master.project/src/main/java/ai4/master/project/Main.java
+++ master.project/src/main/java/ai4/master/project/Main.java
@@ -1,9 +1,14 @@
 package ai4.master.project;
 
+import ai4.master.project.apirequests.RecipeGetterChefkoch;
 import ai4.master.project.process.ExampleRecipe;
 import ai4.master.project.process.ProcessModeler;
 import ai4.master.project.process.ProcessModelerImpl;
+import ai4.master.project.recipe.LANG_FLAG;
 import ai4.master.project.recipe.Recipe;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by René Bärnreuther on 15.05.2017.
@@ -12,11 +17,23 @@ public class Main {
 
     public static void main(String[] args) {
 
+        RecipeGetterChefkoch recipeGetter = RecipeGetterChefkoch.recipeGetterFactory();
+        String ids = recipeGetter.getRecipeIDs("Käse",10);
+
+        String[] id = ids.split(",");
+        List<Recipe> recipes = new ArrayList<Recipe>();
+        for(int i = 0; i < id.length;  i++){
+            Recipe r = new Recipe(LANG_FLAG.DE);
+            recipeGetter.getRecipeIngredients(id[i],r);
+            recipeGetter.getRecipePreparation(id[i],r);
+            recipes.add(r);
+        }
+
         ProcessModeler processModeler = new ProcessModelerImpl();
         //processModeler.convertToProcess(new Recipe());
 
         ProcessModeler example = new ExampleRecipe();
-        example.convertToProcess(new Recipe());
+        example.convertToProcess(new Recipe(LANG_FLAG.DE));
 
     }
 }
