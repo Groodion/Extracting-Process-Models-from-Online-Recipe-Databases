801aa4bd12747d98afa4da062655f727e326637b
Groodion <dominik.Zehrer@gmx.de>
2017-06-29 15:40:39 +0200

    Lib erweitert

--- master.project/src/test/java/ai4/master/project/ParserTest.java
+++ master.project/src/test/java/ai4/master/project/ParserTest.java
@@ -4,12 +4,27 @@ import ai4.master.project.apirequests.RecipeGetterChefkoch;
 import ai4.master.project.process.ProcessModeler;
 import ai4.master.project.process.ProcessModelerImpl;
 import ai4.master.project.recipe.Recipe;
+import ai4.master.project.recipe.Step;
 import ai4.master.project.stanfordParser.Parser;
 
 import java.net.URL;
 
 public class ParserTest {
-/*	public static void main(String[] args) throws Exception {
+
+
+/*    static String[] ids = {"1340271238839144", "997991205154456", "185511079703831", "150681066371674", "1726761281857676", "2397571379105119", "982031203667502", "914011196708021",
+            "1033741208508759", "965881202287446", "43611014899035"};
+*/
+	
+	static String[] ids = {
+		"43611014899035"
+	};
+    public static void main(String[] args) throws Exception {
+    	testParser();
+//    	testProcessModeler();
+    }
+    
+	public static void testParser() throws Exception {
 		XMLLoader loader = new XMLLoader();
 		KeyWordDatabase kwdb = loader.load(new URL("file","","resources/Lib.xml"));
 		Parser parser = new Parser("lib/models/german-fast.tagger");
@@ -17,23 +32,16 @@ public class ParserTest {
 		//1340271238839144
 		//997991205154456,185511079703831,150681066371674,1726761281857676,2397571379105119,982031203667502,914011196708021,1033741208508759,965881202287446,43611014899035,
 
-		Recipe recipe = new RecipeGetterChefkoch().getRecipe("43611014899035");
+		Recipe recipe = new RecipeGetterChefkoch().getRecipe("965881202287446");
 		
 		parser.parseRecipe(recipe);
 		
 		for(Step step : recipe.getSteps()) {
 			System.out.println(step.toEasyToReadString());
 		}
+	}
 
-		ProcessModeler processModeler = new ProcessModelerImpl();
-		processModeler.convertToProcess(recipe);
-	}*/
-
-    static String[] ids = {"1340271238839144", "997991205154456", "185511079703831", "150681066371674", "1726761281857676", "2397571379105119", "982031203667502", "914011196708021",
-            "1033741208508759", "965881202287446", "43611014899035"};
-
-
-    public static void main(String[] args) throws Exception {
+    public static void testProcessModeler() throws Exception {
 
         XMLLoader loader = new XMLLoader();
         KeyWordDatabase kwdb = loader.load(new URL("file", "", "resources/Lib.xml"));
